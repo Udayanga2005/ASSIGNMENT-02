f7d9a7489bd79abc111f08792df93fb20c4162db
Udayanga2005 <ivmathra2point0@gmail.com>
2026-04-21 10:38:20 +0530

    Add files via upload

---
+++ as02.py
@@ -0,0 +1,20 @@
+#expenses stay with parent(age15-20)
+food={"rice":int("RS.300"),
+      "soya meat":int("RS.170"),
+       "mix rice":int("RS.800")
+      }
+clothes={}
+      
+
+print("welcome")
+name=input('What is your name ? ')
+age=int(input("How old are you ? "))
+gender=input('male or female')
+print(name)
+print(age)
+print(gender)
+
+if gender=="male":
+    print("got it you are male")
+else:
+    print("got it you are female")
